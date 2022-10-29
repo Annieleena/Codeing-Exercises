a61f0644fe2ad283e4b83dc4c06e6d223dd81694
Leena Bojja <leena.bojja@outlook.com>
2022-10-29 21:35:23 +0300

    Define JS function bingo

--- jscript.js
+++ jscript.js
@@ -3,3 +3,19 @@ function getTextString() {
 }
 result = getTextString();
 console.log(result);
+
+function checkBingo(num) {
+    if (num > 5) {
+        return 'Bingo!';
+    } else {
+        return 'better Luck next time!';
+    }
+}
+bingoResult = checkBingo(5);
+console.log(bingoResult);
+
+bingoResult = checkBingo(6);
+console.log(bingoResult);
+
+bingoResult = checkBingo(9);
+console.log(bingoResult);
